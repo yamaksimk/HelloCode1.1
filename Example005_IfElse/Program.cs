e739e320c8891e1da76b86398ba779379d17f13b
yamaksimk <mkurakin2001@gmail.com>
2023-09-30 17:29:15 +0300

    IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Привет,зайка");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
