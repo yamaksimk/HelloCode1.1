c38614bf5b2c65b04dbbc44f31909cad9a2cc54c
yamaksimk <mkurakin2001@gmail.com>
2023-10-09 17:51:15 +0300

    intro2

--- Example008_intro/Program.cs
+++ Example008_intro/Program.cs
@@ -11,15 +11,20 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 26443;
 int c2 = 3355;
 int a3 = 13;
 int b3 = 23;
 int c3 = 323;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
